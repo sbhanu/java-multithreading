531d4112ff640e22c663b95210edd6bb39602641
bhanu840 <contactbhanu@gmail.com>
2019-12-06 14:36:50 -0500

    update

--- src/com/java/thread/ThreadCreator.java
+++ src/com/java/thread/ThreadCreator.java
@@ -1,9 +1,7 @@
 package com.java.thread;
 
 
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 /**
  *  Shows various ways to create thread
@@ -16,7 +14,7 @@ public class ThreadCreator extends  Thread{
                 "Created thread by extending java.lang.Thread");
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws  Exception{
 
 
         //Method 1  Create thread by extending java.lang.Thread
@@ -35,32 +33,33 @@ public class ThreadCreator extends  Thread{
         thread2.start();
 
         //Method 3  Create thread by implementing functional interface Runnable
-        Runnable task = ()->{
-            System.out.println(Thread.currentThread().getName()+
-                    "Created thread using runnable task Java 8 functional interface");
-        };
+        Runnable task = ()-> System.out.println(Thread.currentThread().getName()+
+                "Created thread using runnable task Java 8 functional interface");
 
         Thread thread3 = new Thread(task);
         thread3.start();
 
         // Method 4
-        Thread thread4 = new Thread(()->{
-            System.out.println(Thread.currentThread().getName()
-                    +"Created thread using inline runnable task using Java 8 functional interface");
-        });
+        Thread thread4 = new Thread(()-> System.out.println(Thread.currentThread().getName()
+                +"Created thread using inline runnable task using Java 8 functional interface"));
         thread4.start();
 
 
         // Method 5
-        Runnable task2 = ()->{
-
-            System.out.println("Created thread using executor service by passing Runnable functional interface");
-        };
+        Runnable task2 = ()-> System.out.println(Thread.currentThread().getName() + " Created thread using executor service by passing Runnable functional interface");
         ExecutorService executorService = Executors.newSingleThreadExecutor();
         executorService.submit(task2);
 
         // Method 6
-
+        FutureTask<String> future = new FutureTask<String>(()->{
+           System.out.println(Thread.currentThread().getName()+ " Created thread using future task by passing Callable functional interface");
+           return "returnValue";
+        });
+        //ExecutorService service = Executors.newSingleThreadExecutor();
+        //service.submit()
+        Thread t = new Thread(future);
+        t.start();
+        String returnedValue = future.get();
 
     }
 }
